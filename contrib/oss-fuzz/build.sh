3b23167735d95ccab35bb32007030653c402381b
Léonie Louise Dezempte <dwii@eduroam-262-3-008.epfl.ch>
2025-05-07 11:11:35 +0200

    Fix riscv syntax error with portable patch

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -31,7 +31,8 @@ cat scripts/pnglibconf.dfa | \
 mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
 
 # build the libpng library.
-sed -i '' '/riscv\*)/d' configure.ac 
+# Patch riscv* syntax error in configure.ac (portable version)
+grep -v 'riscv*)' configure.ac > configure.ac.patched && mv configure.ac.patched configure.ac
 autoreconf -f -i
 ./configure --with-libpng-prefix=OSS_FUZZ_
 make -j$(nproc) clean
